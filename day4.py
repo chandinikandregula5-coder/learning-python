50729289a5cc09f6f4a307f24dfa75990ea30d6b
chandini <chandinikandregula5@gmail.com>
2026-05-16 12:32:56 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,79 @@
+'''
+concatination
+---------------
+==> the (+) for int and can add, but for the other data types it will act as concatinating the data type
+'''
+a=90
+b=8
+print(a+b)
+
+'''
+tuple
+---------
+-->collection of different data types seperated by commas,represented in () and immutable
+'''
+some = (1, "python" [1,2],(3,4),"python")
+print(some.index(",python"))
+'''
+index()
+---------
+--> used to find out the index position of the item,and only gives the first occurance
+'''
+
+some =(1,[1,2],(3,4),"python")
+print(some.index("python"))
+'''
+dictionary
+--------------
+---->dictionary key and value is seperated by : and pair is seperated by comma
+represented by curly braces{}
+
+eg
+----
+'''
+teja_details = {"Name": "teja",1:2,(1,2):[3,4]}
+print(type teja_details)
+
+'''
+keys()
+-------
+used to get all keys from
+syntax dict.key()
+
+'''
+teja_details={"Name":"teja","age":45,"mobn":"123456789","Pan" "876540976"}
+print(teja_details)              
+'''
+items()
+--------
+used to get key and value together
+
+syntax--> dict.items
+eg
+----
+'''
+teja_details = {"name": "teja",
+                "age":45
+                "mobn":"123456789"
+                "pan":"gpxpb20"}
+
+
+teja_details.update({"adhar":"123456789"})
+print(teja_details)['Name'] = "garikapati"
+
+teja_details.clear()
+print(teja_details)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
