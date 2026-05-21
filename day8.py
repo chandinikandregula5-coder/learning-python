6d1b16d23386d790dd47f6251e45912704007486
chandini <chandinikandregula5@gmail.com>
2026-05-21 15:46:27 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,89 @@
+'''
+time_ = "23.56"
+parts_ = time_.split(":")
+print(f"{time} is converted into {int(parts_[0]-12}:{parts_[1] pm"})
+
+'''
+stu_marks = 22
+if stu_marks >= 90:
+    print("A+")
+elif stu_marks >=80:
+    print("A")
+elif stu_marks >=70:
+    print("B+")
+elif stu_marks >=60:
+    print("B")
+elif stu_marks >=50:
+    print("c+")
+elif stu_marks >=35:
+    print("pass")
+else:
+    print("failed")
+
+
+
+anu = 8
+junnu = 5
+sana = 90
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else:
+    print(c)
+"""
+nested if condition
+
+"""
+SBI_bank ={"ATM PIN": "6600"}
+pin = input("enter 4 digit ATM PIN :")
+if len(pin) == 4:
+    if pin in SBI bank['atm pin']:
+        print("wel come to sbi atm")
+     else
+     
+"""
+any = "python"
+an = [1,2,3,4]
+so = (5,6,7,8)
+for how in any :
+    print(how)
+
+range()
+for i in range (1,100):
+    print(i)
+else:
+    print("code ended here")
+
+syntax------> range(start,end,step)
+range()is in built function
+
+break statement used to exit from the loop based on the condition
+'''
+continue
+----------
+used to skip the current itteration based on the condition
+'''
+for i in range(1,10):
+   if i == 3:
+    print(i)
+
+pass
+    
+for i in range(1,10):
+ if i == 3:
+      pass
+
+
+while is ----> for + if
+while is a combination of for and if
+the loop runs until condition is met or satisfy
+
+      
+  
+
+
+
+
+
+
